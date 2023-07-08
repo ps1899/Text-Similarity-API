8191a6e33322cae2b221af12be60ee5ac20c8ae9
ps1899 <sriprateek1810@gmail.com>
2023-07-08 18:27:15 +0530

    UI changes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -13,14 +13,13 @@ const page: FC = () => {
         <div className='mx-auto flex w-full flex-col justify-center space-y-6 max-w-lg'>
           <div className='flex flex-col items-center gap-6 text-center'>
             <Link
-              href='/'>
-              <a className={buttonVariants({
+              className={buttonVariants({
                 variant: 'ghost',
                 className: 'w-fit',
-              })}>
+              })}
+              href='/'>
               <Icons.ChevronLeft className='mr-2 h-4 w-4' />
               Back to Home
-            </a>
             </Link>
             <LargeHeading>Welcome Back!</LargeHeading>
             <Paragraph>Please Sign-In using your Google account.</Paragraph>
